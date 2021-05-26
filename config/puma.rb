89c0229aed75ffc4e715b3e555944b064edd9926
sanders-man <storozhenkoalex@yandex.ru>
2021-05-26 14:00:44 +0700

    puma fix

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,21 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
+workers 2
+
 port        ENV.fetch("PORT") { 3000 }
 
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+bind "unix://#{shared_dir}/sockets/puma.sock"
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-bind "tcp://#{ENV['IP_ADDR']}:#{ENV['PORT']}"
-plugin :tmp_restart
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
