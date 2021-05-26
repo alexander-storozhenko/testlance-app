1bea2fe646689eadf5f45956750f4bc818516003
sanders-man <storozhenkoalex@yandex.ru>
2021-05-27 00:43:37 +0700

    changed envs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
   schedule_file = "config/schedule.yml"
-  config.redis = {url: "redis://127.0.0.1:6379/0"}
+  config.redis = {url: "redis://#{ENV['REDIS_ADDR']}:#{ENV['REDIS_PORT']}/0"}
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
