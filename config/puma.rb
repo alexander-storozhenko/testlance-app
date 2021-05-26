93e07eb5c74fb8e6f3f42e06a4aa30743f5bc893
sanders-man <storozhenkoalex@yandex.ru>
2021-05-26 14:02:39 +0700

    changed envs

--- config/puma.rb
+++ config/puma.rb
@@ -9,13 +9,3 @@ app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 bind "unix://#{shared_dir}/sockets/puma.sock"
 environment ENV.fetch("RAILS_ENV") { "development" }
-
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
-
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
