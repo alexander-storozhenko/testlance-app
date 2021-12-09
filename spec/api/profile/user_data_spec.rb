68fc1da33887828364e4e7b18116430031907941
kostrulnik <golowko.costya2010@gmail.com>
2021-12-09 18:24:24 +0300

    added user_data unit-test

---
+++ spec/api/profile/user_data_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe API::Profile::UserData do
+  context 'when admin' do
+    let(:path) {'/api/v1/profile/user_data'
+}
+    it 'test' do
+        get path,
+            headers: { 'Access-Token': access_token},
+      expect(response.status).to eq 200
+    end
+  end
+end
